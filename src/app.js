a6b8ace372fde649117ca7858ea1d73e71e8f54b
M-A-N-h-A-S <Connect.Manhas963@gmail.com>
2025-03-02 16:15:02 +0530

    tested get,post,putt and app.use..

--- src/app.js
+++ src/app.js
@@ -3,8 +3,29 @@ const express=require("express");
 const app=express();
 
 app.use("/home",(req,res)=>{
-    res.send("server is active")
+    res.send("use-server is active")
 })
+
+
+app.post("/home",(req,res)=>{
+    res.send("post-server is active")
+})
+app.delete("/home",(req,res)=>{
+    res.send("delete-server is active")
+})
+app.patch("/home",(req,res)=>{
+    res.send("patch-server is active")
+})
+
+
+
+app.get("/home",(req,res)=>{
+    res.send("get-server is active")
+})
+
+
+
+
 
 app.listen(3000,()=>{
     console.log("server is running");
